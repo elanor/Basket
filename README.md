d88b8fd3f68f9e39218fb4f13218c3dc63e287d3
Marina <elanor.me@gmail.com>
2019-10-18 15:57:26 +0300

    Lists updated, creating a new works, close list, mark as done. Colors done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Basket
-a shopping basket application written in pure JS/HTML/CSS
+a shopping basket application written in pure JS/HTML/CSS.
+
+User can create new list, add items, delete items, mark them as done, share.
+
+Vintage color palette is taken from Wes Anderson movies https://www.google.com/url?sa=i&source=imgres&cd=&cad=rja&uact=8&ved=2ahUKEwic4ZGU4aXlAhVGYlAKHbVSBnQQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.are.na%2Fblock%2F2711966&psig=AOvVaw27zxnbD8McqjECQjooMmfH&ust=1571476692566029
---
+++ index.js
@@ -0,0 +1,135 @@
+let listItemCounter = 0;
+const createListItem = itemText => {
+  // generate ids like 'item1', 'item2', ...
+  listItemCounter++;
+  const itemId = "item" + listItemCounter;
+
+  // create container <div class="listItem"/>
+  const listItem = document.createElement("div");
+  listItem.setAttribute("class", "listItem");
+
+  // <input id="item1" type="checkbox" class="checkbox" />
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("id", itemId);
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("class", "checkbox");
+  listItem.appendChild(checkbox);
+
+  // <label for="item1" class="label">Some label</label>
+  const label = document.createElement("label");
+  label.setAttribute("for", itemId);
+  label.setAttribute("class", "label");
+  label.innerText = itemText;
+  listItem.appendChild(label);
+
+  //close button next to each list item, which hides it when clicked
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  listItem.appendChild(span);
+
+  span.onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+
+  return listItem;
+};
+
+const createList = () => {
+  // Get section name
+  const sectionInput = document.querySelector("#sectionText");
+  const listName = sectionInput.value; // get list name from input
+  sectionInput.value = ""; // clear input
+
+  // List
+  const section = document.createElement("section");
+  section.setAttribute("id", 1);
+
+  // List title
+  const h3 = document.createElement("h3");
+  h3.innerText = listName;
+  h3.setAttribute("class", "header");
+  h3.style.marginBottom = "16px";
+  const bckgrndColor = getRandomColor();
+  h3.style.backgroundColor = bckgrndColor;
+  h3.style.color = getContrast(bckgrndColor);
+  section.appendChild(h3);
+
+  // New item input
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "Item...");
+  section.appendChild(input);
+
+  // Create a callback, whhich adds new item
+  const addNewItem = () => {
+    const itemText = input.value;
+    input.value = ""; // clear input
+    const newItem = createListItem(itemText); // create new item
+    section.appendChild(newItem); // append to list
+  };
+
+  // New item button
+  const btn = document.createElement("button");
+  btn.innerText = "Add item";
+  btn.setAttribute("class", "button");
+  btn.style.marginBottom = "10px";
+  btn.onclick = addNewItem;
+  section.appendChild(btn);
+
+  // Share list button
+  const btn_share = document.createElement("button");
+  btn_share.innerText = "Share";
+  btn_share.setAttribute("class", "button");
+  section.appendChild(btn_share);
+
+  // Create new item
+  return section;
+};
+
+const newListButton = document.querySelector(".new-list-button"); // 'New list' button
+const container = document.querySelector("#wrapper"); // app wrapper
+
+newListButton.onclick = () => {
+  try {
+    const section = createList();
+    container.appendChild(section);
+  } catch (e) {
+    alert(e.message);
+  }
+};
+
+// helper function
+const getRandomColor = () => {
+  const colors = [
+    "8c191e", //red brown
+    "e3b110", //golden rod
+    "96712a", //sienna
+    "86b0a4", //dark sea green
+    "e0ccab" //wheat
+  ];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  const color = colors[randomIndex];
+  return "#" + color;
+};
+
+// font color based on a randomly picked background
+const getContrast = hexcolor => {
+  // If a leading # is provided, remove it
+  if (hexcolor.slice(0, 1) === "#") {
+    hexcolor = hexcolor.slice(1);
+  }
+
+  // Convert to RGB value
+  var r = parseInt(hexcolor.substr(0, 2), 16);
+  var g = parseInt(hexcolor.substr(2, 2), 16);
+  var b = parseInt(hexcolor.substr(4, 2), 16);
+
+  // Get YIQ ratio
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+  // Check contrast
+  return yiq >= 128 ? "#36251a" : "#fdebb4"; //dark late gray, mocassin
+};
