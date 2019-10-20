bd42765406cd27ae5ff82866eac1f252b95d5eec
Marina <elanor.me@gmail.com>
2019-10-20 23:33:11 +0300

    sw changed

--- sw.js
+++ sw.js
@@ -6,11 +6,39 @@
     './index.js',
     './manifest.webmanifest'
 ]; */
-self.addEventListener('install', function() {
+self.addEventListener('install', function(event) {
     console.log('SW installed');
+    //event.waitUntil();
+    caches.open('static')
+    .then(function(cache){
+       /*  caches.add('/index.js');
+        caches.add('/');
+        caches.add('/index.html'); */
+        cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './index.js',
+                './manifest.json'
+        ]);
+    })
 });
 
 self.addEventListener('activate', function() {
     console.log('SW activated');
 
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res) {
+            if (res) {
+                return res;
+            } else {
+                return fetch(event.request);
+            }
+        })
+        
+    );
 })
